33bd167ed312189ccc2a85b7cf4c2f147ecd4138
Dinesh Budhathoki <physmarika@gmail.com>
2025-05-28 19:47:25 +0800

    Client footer is done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Auth/LoginPage";
 import ForgotPassword from "./pages/Auth/ForgotPassword";
+import Footer from "./components/shared/Footer";
 
 function App() {
   return (
@@ -15,14 +16,8 @@ function App() {
 
         {/* private Routes */}
       </Routes>
+      <Footer />
       <ToastContainer position="top-center" autoClose={2000} />
-
-      <div className="text-green-700 text-5xl">
-        Home Page Coming soon ...
-        <div className="flex flex-col items-center justify-center min-h-svh">
-          <Button>Click me</Button>
-        </div>
-      </div>
     </>
   );
 }
