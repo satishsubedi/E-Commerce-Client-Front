90adeb25a56ac8f1002edeb63352cc160e0ea266
Shekhar Singh <smehla147@gmail.com>
2025-06-04 12:14:21 +1000

    working on mobile responsive navbar

--- src/components/navbar/MobileNavbar.jsx
+++ src/components/navbar/MobileNavbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 import {
@@ -11,19 +11,23 @@ import {
 } from "@/components/ui/sheet";
 
 const MobileNavbar = () => {
+  const catergotryArray = ["New and Featured", "Men", "Women", "Kids", "Sale"];
+  const [position, setPosition] = useState("left-0");
+  const handleOnClick = () => {
+    position == "left-0" ? setPosition("-left-100") : setPosition("left-0");
+  };
+  console.log(position);
   return (
     <Sheet>
       <SheetTrigger>
         <RxHamburgerMenu className="text-xl font-medium hove:ring hover:ring-4 hover:ring-black text-black dark:text-white cursor-pointer hover:p-2 hover:rounded hover:text-4xl "></RxHamburgerMenu>
       </SheetTrigger>
-      <SheetContent className={"w-50 lg:hidden"}>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+      <SheetContent
+        className={
+          " sm:w-screen p-0 border-2 bg-amber-200 lg:hidden overflow-hidden"
+        }
+      >
+        <SheetDescription>coming.....</SheetDescription>
       </SheetContent>
     </Sheet>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,21 +7,21 @@ import {
 } from "@/components/ui/navigation-menu";
 
 const Navbar = () => {
-  const arr = ["New and Featured", "Men", "Women", "Kids", "Sale"];
+  const catergotryArray = ["New and Featured", "Men", "Women", "Kids", "Sale"];
   return (
     <nav
       className={`gap-4  items-center text-xl flex-wrap font-medium text-black dark:text-white justify-center hidden  lg:flex lg:flex-1`}
     >
       <NavigationMenu className="[data-orientation] = horizental">
         <NavigationMenuList>
-          {arr.map((a, i) => {
+          {catergotryArray.map((category, i) => {
             return (
               <NavigationMenuItem>
                 <NavigationMenuTrigger className="bg-none text-xl hover:bg-orange-600 hover:underline decoration-blue-600 underline-offset-15  delay-300 transition">
-                  {a}
+                  {category}
                 </NavigationMenuTrigger>
                 <NavigationMenuContent className="">
-                  <div className="w-[70vw] h-[300px] ">{a}</div>
+                  <div className="w-[70vw] h-[300px] ">{category}</div>
                 </NavigationMenuContent>
               </NavigationMenuItem>
             );
