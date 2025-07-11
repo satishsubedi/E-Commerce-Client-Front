54db8ee31848795a026501bc619ab03869743d70
Shekhar Singh <smehla147@gmail.com>
2025-07-11 11:52:36 +1000

    setup the categories and products in redux

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 // Navbar.jsx
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {
@@ -10,26 +10,20 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import { getAllCategories } from "../../features/categories/categoriesApi";
+// import { getAllCategories } from "../../axios/categoryAxios";
+import { useSelector } from "react-redux";
+import { RxLetterCaseCapitalize } from "react-icons/rx";
+import { capitalize } from "../../utility/buldCapital";
 
 const Navbar = () => {
-  const [categories, setCategories] = useState([]);
+  const { categories } = useSelector((state) => state.categoriesInfo);
+
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await getAllCategories();
-        setCategories(response?.payload || []);
-      } catch (error) {
-        console.error("Error fetching categories", error);
-      }
-    };
-    fetchCategories();
-  }, []);
-  console.log(categories);
-
   const renderSubCategories = (parentId) => {
-    const subCategories = categories.filter((cat) => cat.parent === parentId);
+    const subCategories = [...categories]?.filter(
+      (cat) => cat.parent === parentId
+    );
     if (subCategories.length === 0) return null;
 
     return (
@@ -48,49 +42,41 @@ const Navbar = () => {
     );
   };
 
-  const topLevelCategories = categories.filter((cat) => cat.parent === null);
-
   return (
-    // <nav
-    //   className={`gap-4  items-center text-xl flex-wrap font-medium text-white dark:text-white justify-center hidden  lg:flex lg:flex-1`}
-    // >
     <nav className="flex gap-4 items-center text-xl flex-wrap font-medium text-white justify-center">
       <NavigationMenu className="[data-orientation] = horizental">
         <NavigationMenuList className="">
-          {topLevelCategories?.map((category) => {
-            return (
-              <NavigationMenuItem key={category._id}>
-                <NavigationMenuTrigger className="bg-slate-900 text-xl hover:underline decoration-blue-600 underline-offset-15 hover:bg-none  delay-300 transition">
-                  {category.name.charAt(0).toUpperCase() +
-                    category.name.slice(1)}
-                </NavigationMenuTrigger>
-                <NavigationMenuContent className="bg-transparent/55  ">
-                  <div className=" bg-transparent w-[100%] flex justify-evenly gap-20 px-40 py-10 ">
-                    {categories
-                      .filter((cat) => cat.parent == category._id)
-                      .map((category) => (
-                        <div key={category._id}>
-                          <h2 className="text-gray-500">
-                            {category.name.charAt(0).toUpperCase() +
-                              category.name.slice(1)}
-                          </h2>
-                          {categories
-                            .filter((cat) => cat.parent == category._id)
-                            .map((c) => {
-                              return (
-                                <div key={c._id}>
-                                  {c.name.charAt(0).toUpperCase() +
-                                    c.name.slice(1)}
-                                </div>
-                              );
-                            })}
-                        </div>
-                      ))}
-                  </div>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            );
-          })}
+          {categories
+            ?.filter((category) => category.parent === null)
+            ?.map((category) => {
+              return (
+                <NavigationMenuItem key={category._id}>
+                  <NavigationMenuTrigger className="bg-slate-900 text-xl hover:underline decoration-blue-600 underline-offset-15 hover:bg-none  delay-300 transition">
+                    {capitalize(category.name)}
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent className="  ">
+                    <div className=" w-[100%] flex justify-evenly gap-20 px-40 py-10 ">
+                      {categories
+                        .filter((cat) => cat.parent == category._id)
+                        .map((category) => (
+                          <div key={category._id}>
+                            <h2 className="text-gray-500">
+                              {capitalize(category.name)}
+                            </h2>
+                            {categories
+                              .filter((cat) => cat.parent == category._id)
+                              .map((c) => {
+                                return (
+                                  <div key={c._id}>{capitalize(c.name)}</div>
+                                );
+                              })}
+                          </div>
+                        ))}
+                    </div>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              );
+            })}
         </NavigationMenuList>
       </NavigationMenu>
     </nav>
