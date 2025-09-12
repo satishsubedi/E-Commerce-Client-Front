8603c5ed950f088f5c5d532ba69313ab666eecd9
satishsubedi <satish12361@gmail.com>
2025-09-12 11:03:50 +0545

    changes in guestid.js

--- src/utils/guestId.js
+++ src/utils/guestId.js
@@ -1,7 +1,8 @@
+import { v4 as uuidv4 } from "uuid";
 export const getOrCreateGuestId = () => {
   let guestId = localStorage.getItem("guestId");
   if (!guestId) {
-    guestId = crypto.randomUUID();
+    guestId = uuidv4;
     localStorage.setItem("guestId", guestId);
   }
   return guestId;
